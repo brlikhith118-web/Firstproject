917d2d875989c405ea9be746febc86f28f5f0051
likhith <brlikhith118@gmail.com>
2026-03-24 14:48:48 +0530

    Initial commit: Added welcome message and greet function

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+
+print("Welcome to Task Manager App 🚀")
+
+def greet_user(name):
+    return f"Hello, {name}! Welcome to the app."
+
+if __name__ == "__main__":
+    print(greet_user("User"))
