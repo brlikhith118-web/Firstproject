3607e3e590d9a0832d8961c11804427fd5a2fc83
likhith <brlikhith118@gmail.com>
2026-03-24 14:53:10 +0530

    Added login feature

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ print("Welcome to Task Manager App 🚀")
 def greet_user(name):
     return f"Hello, {name}! Welcome to the app."
 
+
+# 🔐 Login Feature
+def login(username, password):
+    if username == "admin" and password == "1234":
+        return "Login Successful ✅"
+    return "Invalid Credentials ❌"
+
+
 if __name__ == "__main__":
     print(greet_user("User"))
+    print(login("admin", "1234"))
